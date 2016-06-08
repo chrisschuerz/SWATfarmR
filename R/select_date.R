c599d3c49922a6ca27401e218c83e7fecaf62f14
Christoph Schürz <christoph.schuerz@boku.ac.at>
2016-06-08 11:47:14 +0200

    completely restructured select_date, added column date_rule to input data

--- R/select_date.R
+++ R/select_date.R
@@ -1,89 +1,88 @@
 # select_OPdate(sdl_df, i_op, op_year, prv_date, meta_data, -----------
 #                   tmp_df, pcp_df, mon_thrs)
 select_date <- function(sdl_df, i_op, op_year, prv_date, meta_data, input_lst,
-                        mon_thrs, thrs_rule, pcp_thrs, api_thrs) {
+                        pcp_thrs, api_thrs, ant_d, sub_d) {
 
   pcp_df  <- input_lst$precipitation
   tmp_df  <- input_lst$temperature$index
   api_lst <- input_lst$antecedent_precip
 
-  # Routine to avoid errors if previous line is empty or NA
-  same_date = FALSE
-  if(length(sdl_df$JDN1[i_op - 1]) != 0 ) {
-    if(!is.na(sdl_df$JDN1[i_op - 1])) {
-      if((sdl_df$JDN1[i_op] == sdl_df$JDN1[i_op-1]) &
-         (sdl_df$JDN2[i_op] == sdl_df$JDN2[i_op-1])) {
-        same_date = TRUE
-      }
-    }
-  }
-
-  if(same_date) {
+  if(sdl_df$DATE_RULE == "p"){
     op_date <- convert_jdn2monday(prv_date, op_year, 0)
-  } else if (do.call(thrs_rule, list(sdl_df$MON_1[i_op], mon_thrs))) {
-
-    if(thrs_rule == "<") {
-      prev_mon <- -1
-      next_mon <-  0
-    } else {
-      prev_mon <-  0
-      next_mon <-  1
-    }
-    tmp_op <- tmp_df %>%
-      filter(., YEAR == op_year) %>%
-      filter(., MON >= (sdl_df$MON_1[i_op] + prev_mon) &
-               MON <= (sdl_df$MON_2[i_op] + next_mon)) %>%
-      select(., contains(meta_data$SUB)) %>%
-      .[,1] %>%
-      mean(., na.rm = TRUE)
-
-    jdn_dates <- c(sdl_df$JDN2[i_op],sdl_df$JDN1[i_op])
-    jdn_init  <- (mean(jdn_dates) +
-                    diff(jdn_dates)*tmp_op*(prev_mon + next_mon)) %>%
-      round(., digits = 0)
-
-    pcp_date <- pcp_df %>%
-      filter(., YEAR == op_year) %>%
-      filter(., JDN >= (jdn_init - 5) &
-               JDN <= (jdn_init + 5)) %>%
-      select(., JDN, contains(meta_data$SUB)) %>%
-      rename.col(.,c("JDN", "PCP"))
-
-    api_date <- api_lst %>%
-      .[["SUB"%_%meta_data$SUB]] %>%
-      filter(., YEAR == op_year) %>%
-      filter(., JDN >= (jdn_init - 5) &
-               JDN <= (jdn_init + 5)) %>%
-      select(., JDN, ends_with(meta_data$SOIL)) %>%
-      rename.col(.,c("JDN", "AMC"))
-
-    op_date <- select_opdate(pcp_date, api_date, 3, 25) %>%
-      jdn.to.monday(., op_year, prv_date)
   } else {
-    pcp_date <- pcp_df %>%
-      filter(., YEAR == op_year) %>%
-      filter(., JDN >= (sdl_df$JDN1[i_op] - 5) &
-               JDN <= (sdl_df$JDN2[i_op] + 5)) %>%
-      select(., JDN, contains(meta_data$SUB)) %>%
-      rename.col(.,c("JDN", "PCP"))
+    switch (sdl_df$DATE_RULE,
+            "n" = {
+              jdn_start <- sdl_df$JDN1[i_op]
+              jdn_end   <- sdl_df$JDN2[i_op]
+              ant_days  <- 0
+              sub_days  <- 0
+            }
+            "o" = {
+              jdn_init <- select_jdninit(tmp_df, sdl_df, op_year, i_op, 0, 0,
+                                         meta_data$SUB)
+              jdn_start <- jdn_init
+              jdn_end   <- jdn_init
+              ant_days <- ant_d
+              sub_days <- sub_d
+            }
+            "<" = {
+              jdn_init <- select_jdninit(tmp_df, sdl_df, op_year, i_op, 1, 0,
+                                         meta_data$SUB)
+              jdn_start <- jdn_init
+              jdn_end   <- jdn_init
+              ant_days <- ant_d
+              sub_days <- sub_d
+            }
+            ">" = {
+              jdn_init <- select_jdninit(tmp_df, sdl_df, op_year, i_op, 0, 1,
+                                         meta_data$SUB)
+              jdn_start <- jdn_init
+              jdn_end   <- jdn_init
+              ant_days <- ant_d
+              sub_days <- sub_d
+            }
+    )
 
-    api_date <- api_lst %>%
-      .[["SUB"%_%meta_data$SUB]] %>%
-      filter(., YEAR == op_year) %>%
-      filter(., JDN >= (sdl_df$JDN1[i_op] - 5) &
-               JDN <= (sdl_df$JDN2[i_op] + 5)) %>%
-      select(., JDN, ends_with(meta_data$SOIL)) %>%
-      rename.col(.,c("JDN", "AMC"))
+    pcp_date <- select_timespan(pcp_df, sdl_df, op_year, jdn_start, jdn_end,
+                                ant_days, sub_days, meta_data$SUB, "PCP")
+    api_date <-select_timespan(api_lst[["SUB"%_%meta_data$SUB]],
+                               sdl_df, op_year, jdn_start, jdn_end,
+                               ant_days, sub_days, meta_data$SUB, "API")
 
-    op_date <- select_opdate(pcp_date, api_date, 5, 25) %>%
-      jdn.to.monday(., op_year, prv_date)
+    op_date <- select_opdate(pcp_date, api_date, pcp_thrs, api_thrs) %>%
+      convert_jdn2monday(., op_year, prv_date)
   }
+
   return(op_date)
 }
 
 
-
 ## Subfunctions -----------------------------------------------------------
+select_timespan <- function(weather_df, sdl_df, op_year, jdn_1, jdn_2,
+                            ant_d, sub_d, sel_label, col_label){
+  sel_timespan <- weather_df %>%
+    filter(., YEAR == op_year) %>%
+    filter(., JDN >= (jdn_1 - ant_d) &
+              JDN <= (jdn_2 + sub_d)) %>%
+    select(., JDN, ends_with(comp_label)) %>%
+    rename.col(.,c("JDN", col_label))
+}
+
+select_jdninit <- function(temp_df, sdl_df, op_year, i_op, prev_mon, next_mon,
+                           sel_label){
+  tmp_op <- temp_df %>%
+    filter(., YEAR == op_year) %>%
+    filter(., MON >= (sdl_df$MON_1[i_op] - prev_mon) &
+              MON <= (sdl_df$MON_2[i_op] + next_mon)) %>%
+    select(., contains(sel_label)) %>%
+    mean(.[,1], na.rm = TRUE)
+
+  jdn_dates <- c(sdl_df$JDN2[i_op],sdl_df$JDN1[i_op])
+  jdn_init  <- (mean(jdn_dates) +
+                  diff(jdn_dates)*tmp_op*(prev_mon + next_mon)) %>%
+    round(., digits = 0)
+  return(jdn_init)
+}
 ## select_opdate <- function(pcp_date, api_date, pcp_thrs, amc_thrs)
 ##
 select_opdate <- function(pcp_date, api_date, pcp_thrs, amc_thrs){
@@ -104,7 +103,8 @@ select_opdate <- function(pcp_date, api_date, pcp_thrs, amc_thrs){
   return(op_date)
 }
 
-## jdn.to.monday(jdn_num, year_num, prv_date)
+
+## convert_jdn2monday(jdn_num, year_num, prv_date)
 ## Function takes julian day and year and converts them to integer values of
 ## month and day. Additionally the date is compared to previous date. If the
 ## date is lower than the previous date then it is set to prev_date + 1 before
