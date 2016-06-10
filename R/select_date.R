95797da7d2bb99d9ffa6ea1a9157cbcb9c8a984a
Christoph Schürz <christoph.schuerz@boku.ac.at>
2016-06-10 15:35:21 +0200

    selection types and future pcp added

--- R/select_date.R
+++ R/select_date.R
@@ -1,7 +1,7 @@
 # select_OPdate(sdl_df, i_op, op_year, prv_date, meta_data, -----------
 #                   tmp_df, pcp_df, mon_thrs)
 select_date <- function(sdl_df, i_op, op_year, prv_date, meta_data, input_lst,
-                        thrs, day_rnd) {
+                        thrs, day_rnd, sel_type) {
 
   pcp_df  <- input_lst$precipitation
   tmp_df  <- input_lst$temperature$index
@@ -36,12 +36,20 @@ select_date <- function(sdl_df, i_op, op_year, prv_date, meta_data, input_lst,
             }
     )
 
-    pcp_date <- select_timespan(pcp_df, sdl_df, op_year, jdn_start, jdn_end,
+    pcp_date <- select_timespan(pcp_df, op_year, jdn_start, jdn_end,
                                 day_rnd, meta_data$SUB, "PCP")
-    api_date <-select_timespan(api_lst[["SUB"%_%meta_data$SUB]], sdl_df, op_year,
-                               jdn_start, jdn_end, day_rnd, meta_data$SOIL, "API")
+    pcp_sseq <- select_timespan(pcp_df, op_year, jdn_start, jdn_end,
+                                c(day_rnd[1], day_rnd[2] + n_day),
+                                meta_data$SUB, "PCP") %>%
+      mutate(SSP = compute_subseqprecip(PCP, n_day)) %>%
+      filter(JDN <= pcp_date$JDN) %>%
+      select(-PCP)
+    api_date <- select_timespan(api_lst[["SUB"%_%meta_data$SUB]], op_year,
+                               jdn_start, jdn_end, day_rnd, meta_data$SOIL,
+                               "API")
+    sel_wgt <- compute_sampwghts(day_rnd, sel_type)
 
-    op_date <- select_opdate(pcp_date, api_date, thrs) %>%
+    op_date <- select_opdate(pcp_date, pcp_sseq, api_date, thrs, sel_wgt) %>%
       convert_jdn2monday(., op_year, prv_date)
   }
 
@@ -50,7 +58,7 @@ select_date <- function(sdl_df, i_op, op_year, prv_date, meta_data, input_lst,
 
 
 ## Subfunctions -----------------------------------------------------------
-select_timespan <- function(weather_df, sdl_df, op_year, jdn_1, jdn_2,
+select_timespan <- function(weather_df, op_year, jdn_1, jdn_2,
                             day_rnd, sel_label, col_label){
   sel_timespan <- weather_df %>%
     filter(., YEAR == op_year) %>%
@@ -77,24 +85,45 @@ select_jdninit <- function(temp_df, sdl_df, op_year, i_op, prev_mon, next_mon,
 }
 ## select_opdate <- function(pcp_date, api_date, pcp_thrs, api_thrs)
 ##
-select_opdate <- function(pcp_date, api_date, thrs){
+select_opdate <- function(pcp_date, pcp_sseq, api_date, thrs, sel_wgt){
+
   pcp_sel <- pcp_date %>% filter(., PCP < thrs[1])
-  api_sel <- api_date %>% filter(., API < thrs[2])
-  op_date <- inner_join(pcp_sel, api_sel, by = "JDN")
+  ssp_sel <- pcp_sseq %>% filter(., SSP < thrs[2])
+  api_sel <- api_date %>% filter(., API < thrs[3])
+  op_date <- inner_join(pcp_sel, ssp_sel, by = "JDN") %>%
+    inner_join(., api_sel, by = "JDN")
 
   if(dim(op_date)[1] > 0){
     op_date %<>%
       select(., JDN) %>%
-      sample_n(., 1)
+      sample_n(., 1, weight = sel_wgt)
   } else {
+    wgt <- sum(thrs)/thrs*c(2,1,1) #PCP should have twice the priority to others
     op_date <- inner_join(pcp_date, api_date, by = "JDN") %>%
-      mutate(., WGT = 10*PCP + API) %>%
+      mutate(., WGT = wgt[1]*PCP + wgt[2]*SSP + wgt[3]*API) %>%
       filter(., .$WGT == min(.$WGT)) %>%
       select(., JDN)
   }
   return(op_date)
 }
 
+compute_subseqprecip <- function(pcp_vec, n_day){
+  n <- 1:n_day
+  pcp_sub <- c()
+  for (i in 1:length(pcp_vec)){
+    pcp_sub <- c(pcp_sub, sum(pcp_vec[(i+1):(i+n_day)]/n, na.rm = TRUE))
+  }
+  return(pcp_sub)
+}
+
+compute_sampwghts <- function(day_rnd, sel_type){
+  switch (sel_type,
+          "unif" = {rep(1,(sum(day_rnd)+1))},
+          "norm" = {c((-day_rnd[1]:-1)/day_rnd[1],
+                      (0:day_rnd[2])/day_rnd[2]) %>%
+                    dnorm}
+  )
+}
 
 ## convert_jdn2monday(jdn_num, year_num, prv_date)
 ## Function takes julian day and year and converts them to integer values of
@@ -112,4 +141,6 @@ convert_jdn2monday <- function(jdn_num, year_num, prv_date) {
     as.numeric(.) %>%
     .[2:3]
 }
+
+
 
