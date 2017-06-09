ddfa3e3fc5297f1666fa04f1d16e961f12528fba
chrisschuerz <christoph.schuerz@boku.ac.at>
2017-06-09 10:20:16 +0200

    renaming in writ_luptemp

--- R/write_luctemp.R
+++ R/write_luctemp.R
@@ -1,18 +1,18 @@
-#' Write a template for the luc table of update_landuse()
+#' Write a template for the lup table of update_landuse()
 #'
-#' @param luc_path Either path string to write a .csv or NULL. Then the
+#' @param lup_path Either path string to write a .csv or NULL. Then the
 #'   template structure is returned as a data.frame
 #'
 #' @return
 #' @export
 #'
 #' @examples
-#' # To get a template for the luc_tbl
+#' # To get a template for the lup_tbl
 #'
-#' write_luctemp(choose.dir()) # only windows
-#' write_luctemp() #Returns tibble to .Globalenv
-write_luctemp <- function(luc_path = NULL) {
-  luc_tmp <- data.frame(luse_from = c("FESC", "FESC", "FRST"),
+#' write_luptemp(choose.dir()) # only windows
+#' write_luptemp() #Returns tibble to .Globalenv
+write_luptemp <- function(lup_path = NULL) {
+  lup_tmp <- data.frame(luse_from = c("FESC", "FESC", "FRST"),
                         luse_to   = c("WWHT", "CORN", "CORN"),
                         fraction  = c(0.1, 0.25, 0.05),
                         year_from = c(2010, 2015, 2010),
@@ -21,10 +21,10 @@ write_luctemp <- function(luc_path = NULL) {
                         soil      = c('', '', 'c("soilA", "soilB")'),
                         slope     = c('c("1-3", "3-5")', '"5-9999"', ''),
                         stringsAsFactors = FALSE)
-  if(is.null(luc_path)){
-    return(luc_tmp)
+  if(is.null(lup_path)){
+    return(lup_tmp)
   } else {
-    write.csv(x = luc_tmp, file = paste(luc_path, "luc_tmp.csv", sep = "/"),
+    write.csv(x = lup_tmp, file = paste(lup_path, "lup_tmp.csv", sep = "/"),
               row.names = FALSE)
   }
 }
